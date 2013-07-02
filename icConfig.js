5abdd9da151e6fbc8c92f33e265938bb3204b4a6
coltpini <coltpini@gmail.com>
2013-07-02 14:08:43 -0600

    Init

---
+++ icConfig.js
@@ -0,0 +1,4 @@
+var icConfig = {
+	print : "asdf",
+	tweet : "hjkl"
+};
---
+++ icIcons.js
@@ -0,0 +1,28 @@
+var IcIcons = function(opts) {
+	this.options = {};
+	this.options.config = opts.config || {};
+};
+
+IcIcons.prototype.inject = function(){
+	var conf = this.options.config;
+	for(var icon in conf){
+		var val = conf[icon];
+		if (conf.hasOwnProperty(icon)){
+			var t = val,
+				elems = document.querySelectorAll('ic' + icon),
+				cont = "";
+    			if(val.indexOf(';') > -1){
+				t = val.split(';');
+			}
+			var i = 0;
+			for(i=0; i<t.length;i++){
+				cont += '<span class="icIcon ic' + i + '">' + t[i] + "</span>";
+			}
+			for(i=0; i<elems.length;i++){
+				var elem = elems[i];
+				elem.setAttribute("class", "icContainer");
+				elem.innerHTML = cont;
+			}
+		}
+	}
+};
